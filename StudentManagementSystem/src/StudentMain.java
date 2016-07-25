85705d871cd0b9706ae279b093b11346649c6c96
Ivan Ruiz <ivanruiz82@gmail.com>
2016-07-25 22:06:17 +1200

    Split aryLines into studentContact and displayed

--- StudentManagementSystem/src/StudentMain.java
+++ StudentManagementSystem/src/StudentMain.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 public class StudentMain {
 
 	public static void main(String[] args) {
-		String file_name = "C:\\Users/ivan.ruiz/IT7320_FirstAssignment/StudentManagementSystem/Data/test.txt";
+		String file_name = "data/Studentinfo.txt";
 		try{
 			ReadFile file = new ReadFile(file_name);
 			String[] aryLines = file.OpenFile();
 			int i;
 			for ( i=0; i< aryLines.length; i++) {
-				System.out.println(aryLines[i]);
+				//System.out.println(aryLines[i]);
+				String[] studentContact = aryLines[i].split(", ");
+				System.out.println("Student Name: "+studentContact[0]);
+				System.out.println("Student ID: "+studentContact[1]);
+				System.out.println("Student Contact: "+studentContact[2]);
+				System.out.println("Student Email: "+studentContact[3]);
 			}
 			
 			}
