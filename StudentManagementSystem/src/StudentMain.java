8db7f75285bcdd0d189805e0a855f9d427c083cf
Ivan Ruiz <ivanruiz82@gmail.com>
2016-07-25 22:21:10 +1200

    Got user input and searched for name in text file

--- StudentManagementSystem/src/StudentMain.java
+++ StudentManagementSystem/src/StudentMain.java
@@ -5,6 +5,10 @@ public class StudentMain {
 
 	public static void main(String[] args) {
 		String file_name = "data/Studentinfo.txt";
+		Scanner user_input = new Scanner(System.in);  // Reading from System.in
+		System.out.println("Enter Student's Name: ");
+		String StudentsName = user_input.nextLine( );
+		System.out.println(StudentsName);
 		try{
 			ReadFile file = new ReadFile(file_name);
 			String[] aryLines = file.OpenFile();
@@ -12,10 +16,16 @@ public class StudentMain {
 			for ( i=0; i< aryLines.length; i++) {
 				//System.out.println(aryLines[i]);
 				String[] studentContact = aryLines[i].split(", ");
-				System.out.println("Student Name: "+studentContact[0]);
-				System.out.println("Student ID: "+studentContact[1]);
-				System.out.println("Student Contact: "+studentContact[2]);
-				System.out.println("Student Email: "+studentContact[3]);
+				
+				if(studentContact[0].indexOf(StudentsName) != -1){
+					System.out.println( "Match Found");
+					System.out.println("Student Name: "+studentContact[0]);
+					System.out.println("Student ID: "+studentContact[1]);
+					System.out.println("Student Contact: "+studentContact[2]);
+					System.out.println("Student Email: "+studentContact[3]);
+				}
+					
+				
 			}
 			
 			}
