76c7d208409845faf404d4f8d0e781e609b3d8d1
Ivan Ruiz <ivanruiz82@gmail.com>
2016-07-27 10:19:04 +1200

    Split StudentsName into studentNameSplit. Now find match if middle name not included

--- StudentManagementSystem/src/StudentMain.java
+++ StudentManagementSystem/src/StudentMain.java
@@ -8,25 +8,48 @@ public class StudentMain {
 		Scanner user_input = new Scanner(System.in);  // Reading from System.in
 		System.out.println("Enter Student's Name: ");
 		String StudentsName = user_input.nextLine( );
-		System.out.println(StudentsName);
+		//System.out.println(StudentsName);
+		String[] studentNameSplit = StudentsName.split("\\s+");
 		try{
 			ReadFile file = new ReadFile(file_name);
 			String[] aryLines = file.OpenFile();
 			int i;
+			int s;
+			int NameFound=0;
 			for ( i=0; i< aryLines.length; i++) {
-				//System.out.println(aryLines[i]);
 				String[] studentContact = aryLines[i].split(", ");
+				for (s=0; s< studentNameSplit.length; s++){
+					//System.out.println(aryLines[i]);
+					
+				if(aryLines[i].indexOf(studentNameSplit[s]) != -1){
+					if(studentContact[0].indexOf(studentNameSplit[s]) != -1){
+						NameFound =1;
+					}
+					
+				}
+				else{
+					NameFound=2;
+					
+					s= studentNameSplit.length;
+					
+				}
+				}
 				
-				if(studentContact[0].indexOf(StudentsName) != -1){
+				if(NameFound ==1){
 					System.out.println( "Match Found");
 					System.out.println("Student Name: "+studentContact[0]);
 					System.out.println("Student ID: "+studentContact[1]);
 					System.out.println("Student Contact: "+studentContact[2]);
 					System.out.println("Student Email: "+studentContact[3]);
-				}
+					}
+				
+				
 					
 				
 			}
+			if(NameFound==2){
+				System.out.println("No Match");
+				}
 			
 			}
 		catch (IOException e){
