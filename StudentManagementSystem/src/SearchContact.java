32fa2550b6de4ee6d5541223de07956878b957a5
ivan.ruiz <ivan.ruiz@B102-08.soit.local>
2016-07-29 11:39:17 +1200

    Made StudentMain and the output screen neater. Created SearchContact class to search for contact information and code looks better

---
+++ StudentManagementSystem/src/SearchContact.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SearchContact {
+	public void searchName(){
+		String file_name = "data/Studentinfo.txt";
+		Scanner user_input = new Scanner(System.in);
+		// Reading from System.in
+		System.out.println("Enter Student's Name: "); 
+		String StudentsName = user_input.nextLine( );
+		//System.out.println(StudentsName);
+		String[] studentNameSplit = StudentsName.split("\\s+");
+		try{
+			ReadFile file = new ReadFile(file_name);
+			String[] aryLines = file.OpenFile();
+			int i;
+			int s;
+			int NameFound=0;
+			int matches =0;
+			for ( i=0; i< aryLines.length; i++) {
+				String[] studentContact = aryLines[i].split(", ");
+				for (s=0; s< studentNameSplit.length; s++){
+					//System.out.println(aryLines[i]);
+					
+				if(aryLines[i].indexOf(studentNameSplit[s]) != -1){
+					if(studentContact[0].indexOf(studentNameSplit[s]) != -1){
+						NameFound =1;
+					}
+					
+				}
+				else{
+					NameFound=2;
+					
+					s= studentNameSplit.length;
+					
+				}
+				}
+				
+				if(NameFound ==1){
+					matches++;
+					System.out.println( "--------------------------------");
+					System.out.println("Student Name: "+studentContact[0]);
+					System.out.println("Student ID: "+studentContact[1]);
+					System.out.println("Student Contact: "+studentContact[2]);
+					System.out.println("Student Email: "+studentContact[3]);
+					System.out.println( "--------------------------------");
+					}
+				
+				
+					
+				
+			}
+			if(matches==0){
+				System.out.println("No Match");
+				}
+			else{
+				System.out.println("Number of Matches: "+matches);
+				System.out.println( "--------------------------------");
+			}
+			
+			}
+		catch (IOException e){
+			System.out.println( e.getMessage());
+		}
+	}
+}
--- StudentManagementSystem/src/StudentMain.java
+++ StudentManagementSystem/src/StudentMain.java
@@ -11,9 +11,11 @@ public class StudentMain {
 		Scanner user_int = new Scanner(System.in); 
 		Scanner user_input = new Scanner(System.in);
 		String[] NewStudent= null;
+		
 		System.out.println("Select an option");
-		System.out.println("1: Search for Student Contatc Information");
-		System.out.println("2: Add Student Contact Information");
+		System.out.println("1: Add Student Contact Information");
+		System.out.println("2: Search for Student Contatc Information");
+	
 		System.out.println("3: Exit");
 		 select=user_int.nextInt( );
 		
@@ -35,66 +37,11 @@ public class StudentMain {
 			student1.addcontact();	
 			break;
 		case 2:
-		String file_name = "data/Studentinfo.txt";
-		
-		// Reading from System.in
-		System.out.println("Enter Student's Name: "); 
-		String StudentsName = user_input.nextLine( );
-		//System.out.println(StudentsName);
-		String[] studentNameSplit = StudentsName.split("\\s+");
-		try{
-			ReadFile file = new ReadFile(file_name);
-			String[] aryLines = file.OpenFile();
-			int i;
-			int s;
-			int NameFound=0;
-			int matches =0;
-			for ( i=0; i< aryLines.length; i++) {
-				String[] studentContact = aryLines[i].split(", ");
-				for (s=0; s< studentNameSplit.length; s++){
-					//System.out.println(aryLines[i]);
-					
-				if(aryLines[i].indexOf(studentNameSplit[s]) != -1){
-					if(studentContact[0].indexOf(studentNameSplit[s]) != -1){
-						NameFound =1;
-					}
-					
-				}
-				else{
-					NameFound=2;
-					
-					s= studentNameSplit.length;
-					
-				}
-				}
-				
-				if(NameFound ==1){
-					matches++;
-					System.out.println( "--------------------------------");
-					System.out.println("Student Name: "+studentContact[0]);
-					System.out.println("Student ID: "+studentContact[1]);
-					System.out.println("Student Contact: "+studentContact[2]);
-					System.out.println("Student Email: "+studentContact[3]);
-					System.out.println( "--------------------------------");
-					}
-				
-				
-					
-				
-			}
-			if(matches==0){
-				System.out.println("No Match");
-				}
-			else{
-				System.out.println("Number of Matches: "+matches);
-			}
-			
-			}
-		catch (IOException e){
-			System.out.println( e.getMessage());
-		}
-		break;
+			SearchContact searchcontact= new SearchContact();
+			searchcontact.searchName();
+			break;
 		case 3:
+			System.out.println("GoodBye");
 			break;
 		}
 	}
